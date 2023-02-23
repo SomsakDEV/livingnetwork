903f6291404b200323f69021401eaf1743f2d74a
pongstec <pongstec@ais.co.th>
2023-02-23 14:30:16 +0700

    [MapScreen]add icon and modify code

--- lib/map_screen/locations.g.dart
+++ lib/map_screen/locations.g.dart
@@ -31,25 +31,17 @@ Map<String, dynamic> _$RegionToJson(Region instance) => <String, dynamic>{
     };
 
 Office _$OfficeFromJson(Map<String, dynamic> json) => Office(
-      address: json['address'] as String,
       id: json['id'] as String,
       image: json['image'] as String,
       lat: (json['lat'] as num).toDouble(),
       lng: (json['lng'] as num).toDouble(),
-      name: json['name'] as String,
-      phone: json['phone'] as String,
-      region: json['region'] as String,
     );
 
 Map<String, dynamic> _$OfficeToJson(Office instance) => <String, dynamic>{
-      'address': instance.address,
       'id': instance.id,
       'image': instance.image,
       'lat': instance.lat,
       'lng': instance.lng,
-      'name': instance.name,
-      'phone': instance.phone,
-      'region': instance.region,
     };
 
 Locations _$LocationsFromJson(Map<String, dynamic> json) => Locations(
