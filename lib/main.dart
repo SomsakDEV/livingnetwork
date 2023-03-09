39cc4000b9880755f14698730b6ab05737b1aeee
pongstec <pongstec@ais.co.th>
2023-03-09 14:07:11 +0700

    modify main

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:living_network/model/mode/mode_customer.dart';
 import 'package:living_network/presentation/home/homepage.dart';
-import 'package:living_network/presentation/home/tab_mobile.dart';
-import 'package:living_network/route.dart';
 import 'package:living_network_repository/data/repositories/repositories_impl.dart';
 import 'package:living_network_repository/domain/usecase/get_data_catalog_usecase.dart';
 import 'package:living_network_repository/main.reflectable.dart';
-import 'package:realm/realm.dart';
 import 'package:core/core.dart';
 
 void main() async {
   initializeReflectable();
   WidgetsFlutterBinding.ensureInitialized();
   var coreConfig = CoreConfig(mode: Mode.debug);
-  await coreConfig.checkOrGetConfig().whenComplete(() => IntiAppCionfig()
-      .setInitAppConfig()
-      .whenComplete(() => coreConfig.checkCacheConfig()));
+  await coreConfig.checkOrGetConfig().whenComplete(() => IntiAppCionfig().setInitAppConfig().whenComplete(() => coreConfig.checkCacheConfig()));
   runApp(LivingNetwork());
 }
 
@@ -30,6 +25,7 @@ bool verify = false;
 class _LivingNetworkState extends State<LivingNetwork> {
   static const platform = MethodChannel('LIVING_NETWORK');
   GetDataCatalogUseCase? usecase = GetDataCatalogUseCase(RepositoriesImpl());
+  late final customer;
   String? token;
   late UserData userData;
 
@@ -40,51 +36,56 @@ class _LivingNetworkState extends State<LivingNetwork> {
   }
 
   void _wRequest() async {
-    final customer =
-        await usecase?.getDataAllUserDataWithRealmModel('08123456789');
-
-    if (customer!.isNotEmpty) {
-      var data = customer[0];
-      userData = UserData(
-          msisdn: data.msisdn,
-          networkType: data.networkType,
-          cellId: data.cellId,
-          paymentType: data.paymentType,
-          modelType: data.modelType,
-          customerState: data.customerState,
-          bssrule: data.bssrule,
-          alarm: data.alarm,
-          eco: data.eco);
-      verify = true;
-    } else {
-      throw MissingPluginException();
-    }
-
-    // platform.setMethodCallHandler((MethodCall call) async {
-    //   try {
-    //     print("[LIVING_NETWORK] Command : ${call.method}");
-    //     if (call.method == 'open' && call.arguments != null) {
-    //       print("[LIVING_NETWORK] Input data : ${call.arguments}");
-    //       token = call.arguments;
-    //       verify = true;
-    //     } else {
-    //       throw MissingPluginException();
-    //     }
-    //   } on PlatformException catch (e) {
-    //     print('[LIVING_NETWORK] Error Platform : $e');
-    //   } on MissingPluginException catch (e) {
-    //     print('[LIVING_NETWORK] Missing plugin : $e');
-    //   } catch (e) {
-    //     print('[LIVING_NETWORK] Other error : $e');
-    //   } finally {
-    //     if (verify) {
-    //       platform.invokeMethod('open', ['Success : $token']);
-    //     } else {
-    //       platform.invokeMethod('open', ['Page cant not open']);
-    //       SystemNavigator.pop();
-    //     }
-    //   }
-    // });
+    platform.setMethodCallHandler((MethodCall call) async {
+      try {
+        print("[LIVING_NETWORK] Command : ${call.method}");
+        if (call.method == 'open' && call.arguments != null) {
+          print("[LIVING_NETWORK] Input data : ${call.arguments}");
+          token = call.arguments;
+          if (usecase != null) {
+            //for token query
+            print("[LIVING_NETWORK] usecase : ${usecase.toString()}");
+            customer = await usecase?.getDataAllUserDataWithRealmModel('08123456789');
+            if (customer.isNotEmpty) {
+              print("[LIVING_NETWORK] customer : ${customer.toString()}");
+              var data = customer[0];
+              print("[LIVING_NETWORK] data : ${data.toString()}");
+              userData = UserData(
+                msisdn: data.msisdn ?? "unknown",
+                networkType: data.networkType ?? "unknown",
+                cellId: data.cellId ?? "unknown",
+                paymentType: data.paymentType ?? "unknown",
+                modelType: data.modelType ?? "unknown",
+                customerState: data.customerState ?? "unknown",
+                bssrule: data.bssrule ?? "unknown",
+                alarm: data.alarm ?? "unknown",
+                eco: data.eco ?? "unknown",
+              );
+              verify = true;
+            } else {
+              print('[LIVING_NETWORK] getDataAllUserDataWithRealmModel query fail');
+            }
+          } else {
+            print('[LIVING_NETWORK] GetDataCatalogUseCase is not define');
+          }
+        } else {
+          print('[LIVING_NETWORK] check method or arg');
+        }
+      } on PlatformException catch (e) {
+        print('[LIVING_NETWORK] Error Platform : $e');
+      } on MissingPluginException catch (e) {
+        print('[LIVING_NETWORK] Missing plugin : $e');
+      } catch (e) {
+        print('[LIVING_NETWORK] Other error : $e');
+      } finally {
+        if (verify) {
+          platform.invokeMethod('open', ['Success : $token']);
+        } else {
+          platform.invokeMethod('open', ['Page cant not open']);
+          SystemNavigator.pop();
+        }
+      }
+    });
   }
 
   @override
@@ -92,15 +93,15 @@ class _LivingNetworkState extends State<LivingNetwork> {
     // ignore: prefer_const_constructors
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'DB Heavent'),
       initialRoute: '/',
       routes: {
-        '/': (contxt) => HomePage(
+        '/': (context) => HomePage(
               userData: userData,
             ),
       },
-      theme: ThemeData(fontFamily: 'DB Heavent'),
       // initialRoute: '/livingnetwork/map',
-      onGenerateRoute: (route) => RouteLivingNetwork().generateRoute(route),
+      // onGenerateRoute: (route) => RouteLivingNetwork().generateRoute(route),
     );
   }
 }
