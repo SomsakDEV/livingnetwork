4ff3d86c7bc67a51a8cd1b60320579520646ae7b
pongstec <pongstec@ais.co.th>
2023-03-15 17:12:05 +0700

    reformat tab mobile

--- lib/presentation/home/tab_mobile.dart
+++ lib/presentation/home/tab_mobile.dart
@@ -2,8 +2,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:living_network/component/cell_tower/cell_tower_widget.dart';
-
-// import 'package:living_network/cell_tower_info/ct_main.dart';
 import 'package:living_network/component/internet/usage.dart';
 import 'package:living_network/component/map/map_widget.dart';
 import 'package:living_network/component/mode/mode_widget.dart';
@@ -13,11 +11,7 @@ import 'package:living_network/provider/ln_provider.dart';
 import 'package:living_network/utility/clearData.dart';
 import 'package:provider/provider.dart';
 
-// import 'package:living_network/performance_widget/p_main.dart';
-// import 'package:living_network/speed_comparing/sc_main.dart';
-
 class TabMobile extends StatefulWidget {
-  static const ROUTE_NAME = '/livingnetwork';
 
   TabMobile({
     super.key,
@@ -38,103 +32,81 @@ class _TabMobileState extends State<TabMobile> {
     double w = MediaQuery.of(context).size.width;
     return Consumer<LnProvider>(
       builder: (context, data, _) => RefreshIndicator(
-          color: LNColor.primaryColor,
-          onRefresh: () => data.loadData(),
-          child: SingleChildScrollView(
-            child: Stack(
-              children: <Widget>[
-                Container(
-                  height: h * 0.35,
-                  width: w,
-                  child: MapWidget(),
-                ),
-                InkWell(
-                  onTap: () {
-                    Navigator.pushNamed(context, '/map');
-                  },
-                  child: SizedBox(
-                    height: h * 0.35,
-                    width: w,
-                  ),
-                ),
-                Column(
-                  children: [
-                    SizedBox(
-                      height: h * 0.29,
+        color: LNColor.primaryColor,
+        onRefresh: () => data.loadData(),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Stack(
+                alignment: Alignment.bottomCenter,
+                children: <Widget>[
+                  InkWell(
+                    onTap: () {
+                      Navigator.pushNamed(context, '/map');
+                    },
+                    child: Container(
+                      height: h * 0.35,
                       width: w,
+                      child: MapWidget(),
                     ),
-                    SizedBox(
-                        width: w,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            // Container(
-                            //   decoration: BoxDecoration(
-                            //       color: Color(0xFFFFFFFF),
-                            //       borderRadius: BorderRadius.circular(8),
-                            //       border:
-                            //           Border.all(width: 3, color: Color(0xFFF0F0F0))),
-                            //   width: w * 0.93,
-                            //   child: CellTowerInfoMain(),
-                            // ),
-                            Container(
-                              decoration: BoxDecoration(color: Color(0xFFFFFFFF), borderRadius: BorderRadius.circular(8), border: Border.all(width: 3, color: Color(0xFFF0F0F0))),
-                              width: w * 0.93,
-                              child: CellTowerWidget(),
-                            ),
-                            _sizedBox,
-                            Container(
-                              decoration: BoxDecoration(color: Color(0xFFFFFFFF), borderRadius: BorderRadius.circular(8), border: Border.all(width: 3, color: Color(0xFFF0F0F0))),
-                              width: w * 0.93,
-                              child: PerformanceMain(),
-                            ),
-                            _sizedBox,
-                            // Container(
-                            //   decoration: BoxDecoration(
-                            //       color: Color(0xFFFFFFFF),
-                            //       borderRadius: BorderRadius.circular(8),
-                            //       border:
-                            //           Border.all(width: 3, color: Color(0xFFF0F0F0))),
-                            //   width: w * 0.93,
-                            //   child: SpeedComparingMain(),
-                            // ),
-                            _sizedBox,
-                            Stack(
-                              children: <Widget>[
-                                Padding(
-                                  padding: const EdgeInsets.only(top: 5.0),
-                                  child: Container(
-                                    alignment: Alignment.center,
-                                    decoration: BoxDecoration(color: Color(0xFFFFFFFF), borderRadius: BorderRadius.circular(8), border: Border.all(width: 3, color: Color(0xFFF0F0F0))),
-                                    width: w * 0.93,
-                                    child: ModeWidget(),
-                                  ),
-                                ),
-                                // Positioned(
-                                //   right: 20,
-                                //   child: Image.asset(
-                                //     ImageUtils.getImagePath('assets/images/mode_new_feature.png'),
-                                //     height: 26,
-                                //     width: 149,
-                                //   ),
-                                // ),
-                              ],
-                            ),
-                            _sizedBox,
-                            Container(
-                              decoration: BoxDecoration(color: Color(0xFFFFFFFF), borderRadius: BorderRadius.circular(8), border: Border.all(width: 3, color: Color(0xFFF0F0F0))),
-                              width: w * 0.93,
-                              child: InternetUsage(),
-                            ),
-                            _sizedBox,
-                            ClearData(),
-                          ],
-                        )),
-                  ],
-                )
-              ],
-            ),
-          )),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(color: Color(0xFFFFFFFF), borderRadius: BorderRadius.circular(8), border: Border.all(width: 3, color: Color(0xFFF0F0F0))),
+                    width: w * 0.93,
+                    child: CellTowerWidget(),
+                  ),
+                ],
+              ),
+              _sizedBox,
+              Container(
+                decoration: BoxDecoration(color: Color(0xFFFFFFFF), borderRadius: BorderRadius.circular(8), border: Border.all(width: 3, color: Color(0xFFF0F0F0))),
+                width: w * 0.93,
+                child: PerformanceMain(),
+              ),
+              _sizedBox,
+              // Container(
+              //   decoration: BoxDecoration(
+              //       color: Color(0xFFFFFFFF),
+              //       borderRadius: BorderRadius.circular(8),
+              //       border:
+              //           Border.all(width: 3, color: Color(0xFFF0F0F0))),
+              //   width: w * 0.93,
+              //   child: SpeedComparingMain(),
+              // ),
+              _sizedBox,
+              Stack(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(top: 5.0),
+                    child: Container(
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(color: Color(0xFFFFFFFF), borderRadius: BorderRadius.circular(8), border: Border.all(width: 3, color: Color(0xFFF0F0F0))),
+                      width: w * 0.93,
+                      child: ModeWidget(),
+                    ),
+                  ),
+                  // Positioned(
+                  //   right: 20,
+                  //   child: Image.asset(
+                  //     ImageUtils.getImagePath('assets/images/mode_new_feature.png'),
+                  //     height: 26,
+                  //     width: 149,
+                  //   ),
+                  // ),
+                ],
+              ),
+              _sizedBox,
+              Container(
+                decoration: BoxDecoration(color: Color(0xFFFFFFFF), borderRadius: BorderRadius.circular(8), border: Border.all(width: 3, color: Color(0xFFF0F0F0))),
+                width: w * 0.93,
+                child: InternetUsage(),
+              ),
+              _sizedBox,
+              ClearData(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
