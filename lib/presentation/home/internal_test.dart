1e605424afddac0d54cad30dfbbe68e49b22573d
pongstec <pongstec@ais.co.th>
2023-04-01 22:12:39 +0700

    add check session expire

--- lib/presentation/home/internal_test.dart
+++ lib/presentation/home/internal_test.dart
@@ -104,12 +104,15 @@ class _Mode5GInternalState extends State<Mode5GInternal> {
                 DateTime sExpire = Provider.of<InternalProvider>(context, listen: false).sExpire ?? DateTime.now().add(Duration(hours: 1));
                 print('[LIVING_NETWORK] Session expire : $sExpire');
                 int sec = sExpire.difference(DateTime.now()).inSeconds;
-                duration = Duration(seconds: sec <= 0 ? 0 : sec);
-                timer = Timer.periodic(
-                    const Duration(seconds: 1),
-                    (_) => setState(() {
-                          _counting();
-                        }));
+                if(sec > 0) {
+                  duration = Duration(seconds: sec);
+                  timer = Timer.periodic(
+                      const Duration(seconds: 1),
+                          (_) =>
+                          setState(() {
+                            _counting();
+                          }));
+                }
                 return Dialog(
                   backgroundColor: LNColor.transparent,
                   child: Wrap(
