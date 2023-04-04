c08303d9938c8502ad196b3feba409136c71de62
pongstec <pongstec@ais.co.th>
2023-04-04 16:17:44 +0700

    fix popup

--- lib/presentation/home/internal_test.dart
+++ lib/presentation/home/internal_test.dart
@@ -55,10 +55,7 @@ class _Mode5GInternalState extends State<Mode5GInternal> {
                     }
                   }
                 }
-                if (status) _popup5G;
-                Navigator.pop(context);
-                Provider.of<InternalProvider>(context).setStatus(snap.data as String);
-                return SizedBox();
+                return status ? _popup5G() : SizedBox();
               } else {
                 return SizedBox();
               }
@@ -69,60 +66,52 @@ class _Mode5GInternalState extends State<Mode5GInternal> {
     );
   }
 
-  void _popup5G() {
-    WidgetsBinding.instance.addPostFrameCallback(
-      (_) => showDialog(
-        context: context,
-        barrierDismissible: false,
-        builder: (context) {
-          return Dialog(
-            backgroundColor: LNColor.transparent,
-            child: Wrap(
+  Widget _popup5G() {
+    return Dialog(
+      backgroundColor: LNColor.transparent,
+      child: Wrap(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              color: LNColor.neutralsWhite,
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+            ),
+            child: Column(
               children: [
-                Container(
-                  decoration: const BoxDecoration(
-                    color: LNColor.neutralsWhite,
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                ClipRRect(
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10)),
+                  child: Image.asset(
+                    ImageUtils.getImagePath('assets/images/image.png'),
+                    width: 260,
                   ),
-                  child: Column(
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10)),
-                        child: Image.asset(
-                          ImageUtils.getImagePath('assets/images/image.png'),
-                          width: 260,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 16,
-                      ),
-                      Text('5G Modes!', style: LNStyle.dialogHeader),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      Text('Switch your connection mode to suite\nyour demand the most.', textAlign: TextAlign.center, style: LNStyle.dialogTitleText),
-                      SizedBox(
-                        height: 16,
-                      ),
-                      Button(
-                        textStyle: LNStyle.dialogButtonText,
-                        title: "Got it",
-                        buttonType: ButtonType.primaryBtn,
-                        onPress: () => Navigator.pop(context),
-                        borderRadius: 6,
-                        width: 236,
-                        height: 36,
-                      ),
-                      SizedBox(
-                        height: 16,
-                      ),
-                    ],
-                  ),
-                )
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Text('5G Modes!', style: LNStyle.dialogHeader),
+                SizedBox(
+                  height: 8,
+                ),
+                Text('Switch your connection mode to suite\nyour demand the most.', textAlign: TextAlign.center, style: LNStyle.dialogTitleText),
+                SizedBox(
+                  height: 16,
+                ),
+                Button(
+                  textStyle: LNStyle.dialogButtonText,
+                  title: "Got it",
+                  buttonType: ButtonType.primaryBtn,
+                  onPress: () => Navigator.pop(context),
+                  borderRadius: 6,
+                  width: 236,
+                  height: 36,
+                ),
+                SizedBox(
+                  height: 16,
+                ),
               ],
             ),
-          );
-        },
+          )
+        ],
       ),
     );
   }
